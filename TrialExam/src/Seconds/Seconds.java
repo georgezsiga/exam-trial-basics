ef8f7e99f62a8424046453cde20b0157b451e99c
georgezsiga <georgezsiga@gmail.com>
2017-04-11 09:16:19 +0200

    Add initial file

---
+++ TrialExam/src/Seconds/Seconds.java
@@ -0,0 +1,13 @@
+package Seconds;
+
+/**
+ * Created by georgezsiga on 4/11/17.
+ */
+
+public class Seconds {
+  public static void main(String[] args) {
+    // Create a function that takes a list as a parameter,
+    // and returns a new list with every second element from the orignal list
+    // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+  }
+}
