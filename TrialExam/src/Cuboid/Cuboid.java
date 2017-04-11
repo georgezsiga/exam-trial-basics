90a879df38d6a191cd2ffaa5779d4f2ce69de6ba
georgezsiga <georgezsiga@gmail.com>
2017-04-11 10:48:01 +0200

    Add System Out println to the methods and a main method

--- TrialExam/src/Cuboid/Cuboid.java
+++ TrialExam/src/Cuboid/Cuboid.java
@@ -3,6 +3,10 @@ package Cuboid;
 /**
  * Created by georgezsiga on 4/11/17.
  */
+// Create a class that represents a cuboid:
+// It should take its three dimensions as constructor parameters (numbers)
+// It should have a method called `getSurface` that returns the cuboid's surface
+// It should have a method called `getVolume` that returns the cuboid's volume
 public class Cuboid {
   int length, width, height;
 
@@ -14,16 +18,20 @@ public class Cuboid {
 
   public int getSurface() {
   int surface = 2 * ((length * width) + (width * height) + (height * length));
+    System.out.println("The surface of the cuboid is: " + surface);
   return surface;
   }
 
-  public int volume() {
+  public int getVolume() {
     int volume = length * width * height;
+    System.out.println("The volume of the cuboid is: " + volume);
     return volume;
   }
 
-  // Create a class that represents a cuboid:
-  // It should take its three dimensions as constructor parameters (numbers)
-  // It should have a method called `getSurface` that returns the cuboid's surface
-  // It should have a method called `getVolume` that returns the cuboid's volume
+  public static void main(String[] args) {
+    Cuboid cuboid = new Cuboid(4, 6, 7);
+    cuboid.getSurface();
+    cuboid.getVolume();
+  }
+
 }
