649375c3efde6e447ca62a855cfd3a7cf8071464
georgezsiga <georgezsiga@gmail.com>
2017-04-11 11:40:39 +0200

    Reformat code according to Google Style guide

--- TrialExam/src/PirateCounter/PirateCounter.java
+++ TrialExam/src/PirateCounter/PirateCounter.java
@@ -1,5 +1,7 @@
 package PirateCounter;
+
 import java.util.*;
+
 /**
  * Created by georgezsiga on 4/11/17.
  */
@@ -11,28 +13,24 @@ public class PirateCounter {
 
   public String hasNoLegAndGold(ArrayList arrayList) {
     String piratesWithNoLegAndWithGold = "";
-    for (int i = 0; i < arrayList.size() ; i++) {
+    for (int i = 0; i < arrayList.size(); i++) {
       Pirate pirate = (Pirate) arrayList.get(i);
       if (pirate.hasWoodenLeg && (pirate.getGold() > 15)) {
-                piratesWithNoLegAndWithGold = piratesWithNoLegAndWithGold + " " + pirate.getName();
+        piratesWithNoLegAndWithGold = piratesWithNoLegAndWithGold + " " + pirate.getName();
       }
     }
     System.out.println(piratesWithNoLegAndWithGold);
     return piratesWithNoLegAndWithGold;
   }
 
-  public static void main(String... args){
+  public static void main(String... args) {
     PirateCounter pirateCounter = new PirateCounter();
     ArrayList<Pirate> pirates = new ArrayList<>();
-
-    // Given this list...
-
     pirates.add(new Pirate("Olaf", false, 12));
     pirates.add(new Pirate("Uwe", true, 9));
     pirates.add(new Pirate("Jack", true, 16));
     pirates.add(new Pirate("Morgan", false, 17));
     pirates.add(new Pirate("Hook", true, 20));
-
     pirateCounter.hasNoLegAndGold(pirates);
   }
 }
