068eb2a04fe1d362197ce17f3ffa2cacfe3af9c9
georgezsiga <georgezsiga@gmail.com>
2017-04-11 10:36:05 +0200

    Add constructor and fields

--- TrialExam/src/Cuboid/Cuboid.java
+++ TrialExam/src/Cuboid/Cuboid.java
@@ -4,6 +4,14 @@ package Cuboid;
  * Created by georgezsiga on 4/11/17.
  */
 public class Cuboid {
+  int length, width, height;
+
+  public Cuboid(int length, int width, int height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+
   // Create a class that represents a cuboid:
   // It should take its three dimensions as constructor parameters (numbers)
   // It should have a method called `getSurface` that returns the cuboid's surface
