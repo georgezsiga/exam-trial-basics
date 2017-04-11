35c9fb09485c67c15292b7bf0d6bf3dea46d49b5
georgezsiga <georgezsiga@gmail.com>
2017-04-11 11:39:11 +0200

    Add leg and gold counter and add caller into main method

--- TrialExam/src/PirateCounter/PirateCounter.java
+++ TrialExam/src/PirateCounter/PirateCounter.java
@@ -3,33 +3,36 @@ import java.util.*;
 /**
  * Created by georgezsiga on 4/11/17.
  */
+// Write a function that takes any list that contains pirates as in the example,
+// And returns a list of names containing the pirates that
+// - have wooden leg and
+// - have more than 15 gold
 public class PirateCounter {
-  class Pirate {
-    String name;
-    boolean hasWoodenLeg;
-    int gold;
 
-    Pirate(String name, boolean hasWoodenLeg, int gold) {
-      this.name = name;
-      this.hasWoodenLeg = hasWoodenLeg;
-      this.gold = gold;
+  public String hasNoLegAndGold(ArrayList arrayList) {
+    String piratesWithNoLegAndWithGold = "";
+    for (int i = 0; i < arrayList.size() ; i++) {
+      Pirate pirate = (Pirate) arrayList.get(i);
+      if (pirate.hasWoodenLeg && (pirate.getGold() > 15)) {
+                piratesWithNoLegAndWithGold = piratesWithNoLegAndWithGold + " " + pirate.getName();
+      }
     }
+    System.out.println(piratesWithNoLegAndWithGold);
+    return piratesWithNoLegAndWithGold;
   }
 
   public static void main(String... args){
+    PirateCounter pirateCounter = new PirateCounter();
     ArrayList<Pirate> pirates = new ArrayList<>();
 
     // Given this list...
 
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
 
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
+    pirateCounter.hasNoLegAndGold(pirates);
   }
 }
