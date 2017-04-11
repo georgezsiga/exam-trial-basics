edbaac84b15b3813db51a7d30b348a987a89cb2b
georgezsiga <georgezsiga@gmail.com>
2017-04-11 11:05:13 +0200

    Add number of As method

--- TrialExam/src/CountAs/CountAs.java
+++ TrialExam/src/CountAs/CountAs.java
@@ -5,12 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by georgezsiga on 4/11/17.
  */
 public class CountAs {
+
+  public CountAs() {
+
+  }
 
   public List<String> readFile(String filename) {
     List<String> linesInTheFile = new ArrayList<>();
@@ -25,17 +30,50 @@ public class CountAs {
     return linesInTheFile;
   }
 
-  public int checkTheAs(String string) {
+  public HashMap<Character, Integer> numberOfAs(String string) {
+    String lettersFromFile = readFile(string).toString();
+    HashMap<Character, Integer> map = new HashMap<>();
+    for (char ch : lettersFromFile.toCharArray()) {
+      if (map.containsKey("a")) {
+        int val = map.get(ch);
+        map.put(ch, val + 1);
+      } else {
 
-    return numberOfAs;
+      }
+    }
+    System.out.println(map);
+    return map;
   }
 
+
+
+
+
+//  public int checkTheAs(String string) {
+//    String lettersFromFile = readFile(string).toString();
+//    int numberOfAs = 0;
+//    char temp =;
+//    for (int i = 0; i < lettersFromFile.length() ; i++) {
+//      Character.isLetter()
+//        temp = lettersFromFile.charAt(i);
+//if (Character.isLetter(temp) ) {
+//
+//}
+//    }
+//
+//    return numberOfAs;
+//  }
+
   public static void main(String[] args) {
+    CountAs countAs = new CountAs();
+    countAs.numberOfAs("afile.txt");
+//    countAs.checkTheAs("afile.txt");
     // Create a function that takes a filename as string parameter,
     // counts the occurances of the letter "a" in the file, and returns it as a number.
     // If the file does not exist, the function should return 0 and not break.
 
     // example: on the input "afile.txt" the function should return 28 - print this result
     // example: on the input "not-a-file" the function should return 0 - print this result
+
   }
 }
