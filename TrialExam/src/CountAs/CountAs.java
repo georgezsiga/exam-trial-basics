2eb23c52ac4707d4d1be1d87acf6291cf2e7858f
georgezsiga <georgezsiga@gmail.com>
2017-04-11 09:50:12 +0200

    Add the read the file method

--- TrialExam/src/CountAs/CountAs.java
+++ TrialExam/src/CountAs/CountAs.java
@@ -1,12 +1,38 @@
 package CountAs;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by georgezsiga on 4/11/17.
  */
 public class CountAs {
+
+  public List<String> readFile(String filename) {
+    List<String> linesInTheFile = new ArrayList<>();
+    try {
+      Path filePath = Paths.get(filename);
+      linesInTheFile = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("0");
+      System.exit(0);
+    }
+    return linesInTheFile;
+  }
+
+  public int checkTheAs(String string) {
+
+    return numberOfAs;
+  }
+
   public static void main(String[] args) {
     // Create a function that takes a filename as string parameter,
-    // counts the occurances of the letter "a", and returns it as a number.
+    // counts the occurances of the letter "a" in the file, and returns it as a number.
     // If the file does not exist, the function should return 0 and not break.
 
     // example: on the input "afile.txt" the function should return 28 - print this result
