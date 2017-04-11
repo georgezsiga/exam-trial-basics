b3a38c6406915aaf11f6483279a34f563db38862
georgezsiga <georgezsiga@gmail.com>
2017-04-11 09:31:54 +0200

    Add the initial list

--- TrialExam/src/Seconds/Seconds.java
+++ TrialExam/src/Seconds/Seconds.java
@@ -1,13 +1,24 @@
 package Seconds;
 
+import java.util.ArrayList;
+
 /**
  * Created by georgezsiga on 4/11/17.
  */
 
 public class Seconds {
+
+
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+
   }
 }
