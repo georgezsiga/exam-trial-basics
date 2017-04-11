10b8bc7d2e5b591a3f18defa187a17970a4dccea
georgezsiga <georgezsiga@gmail.com>
2017-04-11 11:18:49 +0200

    Add initial file Pirate class

---
+++ TrialExam/src/PirateCounter/Pirate.java
@@ -0,0 +1,16 @@
+package PirateCounter;
+
+/**
+ * Created by georgezsiga on 4/11/17.
+ */
+public class Pirate {
+  String name;
+  boolean hasWoodenLeg;
+  int gold;
+
+  public Pirate(String name, boolean hasWoodenLeg, int gold) {
+    this.name = name;
+    this.hasWoodenLeg = hasWoodenLeg;
+    this.gold = gold;
+  }
+}
